5de31918eeed6ad95387ab5296063959ab52eba4
carlos0550 <carlospelinski03@gmail.com>
2024-11-21 14:50:20 -0300

    Agrega tallas a los productos

--- src/utils/tallas.js
+++ src/utils/tallas.js
@@ -15,4 +15,40 @@ export const tallas = [
         id: 4,
         name: "XL"
     },
+    {
+        id: 5,
+        name: "36"
+    },
+    {
+        id: 6,
+        name: "37"
+    },
+    {
+        id: 7,
+        name: "38"
+    },
+    {
+        id: 8,
+        name: "39"
+    },
+    {
+        id: 9,
+        name: "40"
+    },
+    {
+        id: 10,
+        name: "41"
+    },
+    {
+        id: 11,
+        name: "42"
+    },
+    {
+        id: 12,
+        name: "43"
+    },
+    {
+        id: 13,
+        name: "44"
+    },
 ]
